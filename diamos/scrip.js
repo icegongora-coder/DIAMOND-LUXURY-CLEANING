0fc9c74448e870f576f495d60d67e6ae41334eed
icegongora-coder <icegongora@gmail.com>
2026-04-01 00:43:06 -0500

    Add files via upload

---
+++ diamos/scrip.js
@@ -0,0 +1,135 @@
+body {
+margin: 0;
+font-family: Arial;
+background: #0d0d0d;
+color: white;
+}
+
+/* HEADER */
+header {
+display: flex;
+justify-content: space-between;
+padding: 20px;
+background: black;
+color: gold;
+}
+
+nav a {
+color: gold;
+margin-left: 15px;
+text-decoration: none;
+}
+
+/* HERO */
+.hero {
+text-align: center;
+padding: 120px 20px;
+background: linear-gradient(black, #1a1a1a);
+}
+
+.hero h2 {
+font-size: 40px;
+}
+
+/* BOTÓN */
+.btn {
+background: gold;
+color: black;
+padding: 12px 25px;
+text-decoration: none;
+display: inline-block;
+margin-top: 20px;
+}
+
+/* SECCIONES */
+.section {
+padding: 60px 20px;
+text-align: center;
+}
+
+.dark {
+background: #1a1a1a;
+}
+
+/* TARJETAS */
+.cards {
+display: flex;
+justify-content: center;
+gap: 20px;
+flex-wrap: wrap;
+}
+
+.card {
+background: #222;
+padding: 20px;
+width: 250px;
+border: 1px solid gold;
+transition: 0.3s;
+}
+
+.card:hover {
+transform: scale(1.05);
+}
+
+/* GALERÍA */
+.gallery {
+display: flex;
+gap: 10px;
+justify-content: center;
+flex-wrap: wrap;
+}
+
+.gallery img {
+width: 300px;
+border-radius: 10px;
+}
+
+/* FORMULARIO */
+form {
+display: flex;
+flex-direction: column;
+max-width: 400px;
+margin: auto;
+}
+
+input, textarea {
+margin: 10px 0;
+padding: 10px;
+border: none;
+}
+
+/* BOTÓN FORM */
+button {
+background: gold;
+color: black;
+padding: 12px;
+border: none;
+}
+
+/* WHATSAPP */
+.whatsapp {
+position: fixed;
+bottom: 20px;
+right: 20px;
+background: green;
+color: white;
+padding: 15px;
+border-radius: 50%;
+text-decoration: none;
+font-size: 20px;
+}
+
+/* FOOTER */
+footer {
+text-align: center;
+padding: 20px;
+background: black;
+color: gold;
+}
+
+/* RESPONSIVE (CELULAR) */
+@media (max-width: 768px) {
+.hero h2 {
+font-size: 28px;
+}
+}
